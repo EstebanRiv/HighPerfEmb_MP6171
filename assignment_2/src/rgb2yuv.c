ea28d1ac574a8c8f19acddf63e136e467ed72779
fparisk <fpariskikut@gmail.com>
2019-06-22 13:14:08 -0600

    rgb2yuv_functional _v1

--- assignment_2/src/rgb2yuv.c
+++ assignment_2/src/rgb2yuv.c
@@ -91,20 +91,25 @@ void rgb2yuv(char *input_image, char *output_image)
     ptr = fopen(input_image,"rb");
     fread(buffer_in,n_bytes,1,ptr);
 
-    if (!ptr)
+    if (ptr != NULL)
     {
-        // Input format: RGB24 -->|B G R|B G R|B G R|....|B G R|B G R|B G R|<-- high memory
-        
-        /* Este es la formula/algoritmo que sale en todo lado, No Funciona!
-           Nuevo approach (basado en https://github.com/descampsa/yuv2rgb/blob/master/yuv_rgb.c) en progreso
+        // Input format: RGB24, low memory-->|B G R|B G R|B G R|....|B G R|B G R|B G R|<--high memory
+        // Output format: YUV444 packed, low memory->|Y Y Y Y Y..|U U U U U...|V V V V V|<--high memory
            
         // i=B, i+1=G, i+2=R
         for(int i = 0; i<n_bytes-3; 3*i++)
         {            
-            buffer_out[i]   = (( buffer_in[i+2]*66  + buffer_in[i+1]* 129 + buffer_in[i]*25  + 128) >> 8) + 16;            // Y =  R*0.299000 + G*0.587000 + B*0.114000
-            buffer_out[i+1] = ((-buffer_in[i+2]*38  - buffer_in[i+1]* 74 + buffer_in[i]*112  + 128) >> 8) + 128;           // U = -R*0.168736 - G*0.331264 + B*0.500000 + 128
-            buffer_out[i+2] = (( buffer_in[i+2]*112 - buffer_in[i+1]* 94 - buffer_in[i]*18   + 128) >> 8) + 128;           // V =  R*0.500000 - G*0.418688 - B*0.081312 + 128
-        } */
+            buffer_out[i]   = (( buffer_in[i+2]*66  + buffer_in[i+1]* 129 + buffer_in[i]*25  + 128) >> 8) + 16;                        // Y =  R*0.299000 + G*0.587000 + B*0.114000
+            buffer_out[i+(n_bytes/3)] = ((-buffer_in[i+2]*38  - buffer_in[i+1]* 74 + buffer_in[i]*112  + 128) >> 8) + 128;             // U = -R*0.168736 - G*0.331264 + B*0.500000 + 128
+            buffer_out[i+(2*n_bytes/3)] = (( buffer_in[i+2]*112 - buffer_in[i+1]* 94 - buffer_in[i]*18   + 128) >> 8) + 128;           // V =  R*0.500000 - G*0.418688 - B*0.081312 + 128
+        }
+
+        for(int i = 0; i<n_bytes; i++)
+        {            
+            printf("%i\n", buffer_out[i]);
+            printf("\n");
+        }
+         
     }
 
     write_ptr = fopen(output_image,"wb");
