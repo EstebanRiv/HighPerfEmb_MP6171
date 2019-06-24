87b5902444b27d330df0bd4340de09ee28bc1542
kevinsteve17 <kevinsteve17@gmail.com>
2019-06-23 21:35:16 -0700

    Fixed some data types in intrinsics

--- assignment_1/Parte2/meta-tec/recipes-rgb2yuv_intrinsics/files/rgb2yuv_intrinsics-1.0/rgb2yuv_intrinsics.c
+++ assignment_1/Parte2/meta-tec/recipes-rgb2yuv_intrinsics/files/rgb2yuv_intrinsics-1.0/rgb2yuv_intrinsics.c
@@ -83,7 +83,7 @@ void rgb2yuv(char *input_image, char *output_image)
     int heigth = 640;
     int width = 480;
 
-    int n_bytes = heigth * width * 3;
+    uint32_t n_bytes = heigth * width * 3;
 
     unsigned char buffer_in[n_bytes];
     unsigned char buffer_out[n_bytes];
@@ -98,44 +98,46 @@ void rgb2yuv(char *input_image, char *output_image)
         // Output format: YUV444 packed, low memory->|Y Y Y Y Y..|U U U U U...|V V V V V|<--high memory
            
         // i=B, i+1=G, i+2=R
-        for(int i = 0; i<n_bytes-3; 3*i++)
+        for(uint32_t i = 0; i<n_bytes-3; 3*i++)
         {
+
+          
 
           uint32x4_t twoahead =  vaddq_u32(i,2);
           uint32x4_t oneahead =  vaddq_u32(i,1);
           int64x2_t sixtysixTimes_2 = vmull_s32 (buffer_in[twoahead], 66);
           int64x2_t onetwonineTimes_1 = vmull_s32(buffer_in[oneahead], 129);
           int64x2_t twentyfiveTimes = vmull_s32(buffer_in[i],25);
-          uint32x4_t firstinternalSum =  vaddq_u32(vaddq_u32(sixtysixTimes_2  + onetwonineTimes_1) + vaddq_u32(twentyfiveTimes, 128));
-          int32x2_t shiftleft_1 = vshl_n_s32 (internalSum, 8);
+          uint32x2_t firstinternalSum =  vaddq_u32(vaddq_u32(sixtysixTimes_2  + onetwonineTimes_1) + vaddq_u32(twentyfiveTimes, 128));
+          int32x2_t shiftleft_1 = vshl_n_s32 (firstinternalSum, 8);
 
           buffer_out[i]   = vaddq_u32(shiftleft_1, 16);                        // Y =  R*0.299000 + G*0.587000 + B*0.114000
 
 
 
-          int64x2_t thirtyeightTimes_2 = vmull_s32 (buffer_in[twoahead], 38);
-          int64x2_t seventyfourTimes_1 = vmull_s32 (buffer_in[oneahead], 74);
-          int64x2_t onetwelveTimes_1 = vmull_s32 (buffer_in[oneahead], 112);
-          int32x2_t negative = vmull_s32(thirtyeightTimes_2,-1);
-          int64x2_t onetwelveTimes = vmull_s32(buffer_in[i],112);
-          uint32x4_t SecondinternalSum =  vaddq_u32( vsubq_s32(negative, seventyfourTimes_1), vaddq_u32(onetwelveTimes, 128));
-          int32x2_t shiftleft_2 = vshl_n_s32 (SecondinternalSum, 8);
-          uint32x2_t index_2 = vadd_u32(i, n_bytes/3);
+          // int64x2_t thirtyeightTimes_2 = vmull_s32 (buffer_in[twoahead], 38);
+          // int64x2_t seventyfourTimes_1 = vmull_s32 (buffer_in[oneahead], 74);
+          // int64x2_t onetwelveTimes_1 = vmull_s32 (buffer_in[oneahead], 112);
+          // int32x2_t negative = vmull_s32(thirtyeightTimes_2,-1);
+          // int64x2_t onetwelveTimes = vmull_s32(buffer_in[i],112);
+          // uint32x4_t SecondinternalSum =  vaddq_u32( vsubq_s32(negative, seventyfourTimes_1), vaddq_u32(onetwelveTimes, 128));
+          // int32x2_t shiftleft_2 = vshl_n_s32 (SecondinternalSum, 8);
+          // // // uint32x2_t index_2 = vadd_u32(i, n_bytes/3);
 
-          buffer_out[index_2] = vaddq_u32(shiftleft_2, 128;             // U = -R*0.168736 - G*0.331264 + B*0.500000 + 128
-
-
-
-          int64x2_t onetwelveTimes_2 = vmull_s32 (buffer_in[twoahead], 112);
-          int64x2_t ninetyfourTimes_1 = vmull_s32(buffer_in[oneahead], 94);
-          int64x2_t eighteenTimes = vmull_s32(buffer_in[i],18);
-          uint32x4_t ThirdinternalSum =  vsubq_s32( vsubq_s32(onetwelveTimes_2, ninetyfourTimes_1), vaddq_u32(eighteenTimes, 128));
-          int32x2_t shiftleft_3 = vshl_n_s32 (ThirdinternalSum, 8);
-          int64x2_t subIndex = vmull_s32(2,n_bytes/3);
-          uint32x2_t index_3 = vadd_u32(i, subIndex);
+          // buffer_out[i, n_bytes/3] = vaddq_u32(shiftleft_2, 128;             // U = -R*0.168736 - G*0.331264 + B*0.500000 + 128
 
 
-          buffer_out[index_3] = vaddq_u32(shiftleft_3, 128;)           // V =  R*0.500000 - G*0.418688 - B*0.081312 + 128
+
+          // int64x2_t onetwelveTimes_2 = vmull_s32 (buffer_in[twoahead], 112);
+          // int64x2_t ninetyfourTimes_1 = vmull_s32(buffer_in[oneahead], 94);
+          // int32x4_t eighteenTimes = vmull_s32(buffer_in[i],18);
+          // uint32x4_t ThirdinternalSum =  vsubq_s32( vsubq_s32(onetwelveTimes_2, ninetyfourTimes_1), vaddq_u32(eighteenTimes, 128));
+          // int32x2_t shiftleft_3 = vshl_n_s32 (ThirdinternalSum, 8);
+          // // // int64x2_t subIndex = vmull_s32(2,n_bytes/3);
+          // // // uint32x2_t index_3 = vadd_u32(i, subIndex);
+
+
+          // buffer_out[i, 2,n_bytes/3] = vaddq_u32(shiftleft_3, 128;)           // V =  R*0.500000 - G*0.418688 - B*0.081312 + 128
 
 
 
